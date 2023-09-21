61151fbb9c50572795caccf29cd357b3581ad72b
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-22 01:27:55 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,66 @@
+
+if __name__ == "__main__":
+import random
+def main():
+    data = generate_random_data()
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    return data
+
+def generate_random_data():
+
+
+    for item in data:    for item in data:
+def main():
+def generate_random_data():
+
+if __name__ == "__main__":
+import random
+    main()
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def main():
+
+
+if __name__ == "__main__":
+import random
+def generate_random_data():
+    return data
+        print(f"Random Number: {item}")
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+if __name__ == "__main__":
+def main():
+    data = generate_random_data()
+
+    return data
+
+        print(f"Random Number: {item}")
+
+
+    main()
+    for item in data:
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import randomimport random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def main():
+
+    main()
+        print(f"Random Number: {item}")
+    return data
+
+if __name__ == "__main__":
+    data = generate_random_data()
+
+    for item in data:
+def generate_random_data():
